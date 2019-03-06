9d46ed8e133cb4977edfa46723f7be8de72d8273
WP Capsules <wpcapsules@WPs-MacBook-Pro.local>
2019-03-06 16:41:09 +0545

    Updates

---
+++ precache-manifest.b23cc91b892f78916d6060345922396a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "22f31645cbebd8fe20a7",
+    "url": "/simpleTodoList/static/js/runtime~main.22f31645.js"
+  },
+  {
+    "revision": "fc8e6d0889917d46e1fb",
+    "url": "/simpleTodoList/static/js/main.fc8e6d08.chunk.js"
+  },
+  {
+    "revision": "732cc5b16681b8413208",
+    "url": "/simpleTodoList/static/js/2.732cc5b1.chunk.js"
+  },
+  {
+    "revision": "f1563d6587b13b465e9e61a6278959a4",
+    "url": "/simpleTodoList/index.html"
+  }
+];
